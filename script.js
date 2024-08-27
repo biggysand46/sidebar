0e4c88fd39477272f940b6b8f6805d36133c114e
biggysand46 <130106790+biggysand46@users.noreply.github.com>
2024-08-27 17:00:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function sidebar_animation() {
+    document.getElementsByClassName("page")[0].style.animation = "sidebar-out 0.4s ease-out forwards"
+    document.getElementsByClassName("slide")[0].style.animation = "slide-out 0.4s ease-out forwards"
+}
